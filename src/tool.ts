9b004f8694dca251576951e49c72ff8ce8b9958b
songbinghui <soongbinghui@gmail.com>
2023-08-31 23:17:51 +0800

    feat: 删除无用函数

--- src/tool.ts
+++ src/tool.ts
@@ -1,22 +1,5 @@
 import path from 'node:path';
 import fs from 'fs-extra';
-import validateNpmPackageName from 'validate-npm-package-name';
-import { Framework, FrameworkVariant } from './type';
-
-/** 删除空格和结尾/ */
-export const formatTargetDir = (targetDir: string) => {
-  return targetDir.trim().replace(/\s+/g, '-').replace(/\/+$/g, '') ?? targetDir;
-};
-
-/** 将用户输入的项目名称转换为一个符合命名规范的字符串 */
-export const toValidPackageName = (projectName: string) => {
-  return projectName
-    .trim()
-    .toLowerCase()
-    .replace(/\s+/g, '-') // 匹配所有空格，并用连字符 - 替换
-    .replace(/^[._]/, '') // 匹配开头的点号或下划线，将其删除
-    .replace(/[^a-z\d\-~]+/g, '-'); // 匹配所有非小写字母、数字、连字符、波浪线的字符，并用连字符 - 替换
-};
 
 /** 目录是否为空 */
 export const isEmptyDir = (path: string) => {
